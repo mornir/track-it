f3a4f0142862d7e42ff496e1cce2935c2bdb9a05
mornir <jeromepott@hotmail.com>
2020-10-03 20:38:47 +0200

    Add test for streak function

--- tests/functions/history.test.js
+++ tests/functions/history.test.js
@@ -1,5 +1,9 @@
-import getLatestVisitedSite from '../../src/utils/getLatestVisitedSite'
-import historySearch from '../fixtures/historySearch.json'
+import {
+  getLatestVisitedSite,
+  getLongestStreak,
+} from '../../src/utils/getHistoryFunctions'
+import historyItems from '../fixtures/historyItems.json'
+import visitItems from '../fixtures/visitItems.json'
 
 it('returns false if there are no history results', () => {
   expect(getLatestVisitedSite()).toBe(false)
@@ -7,5 +11,16 @@ it('returns false if there are no history results', () => {
 })
 
 it('returns the latest visited site among the provided urls', () => {
-  expect(getLatestVisitedSite(historySearch).id).toBe('31583')
+  expect(getLatestVisitedSite(historyItems).id).toBe('31583')
+})
+
+it('verifies the properties of the streak object', () => {
+  const { firstTimestamp, secondTimestamp, startDate, diff } = getLongestStreak(
+    visitItems
+  )
+  expect(startDate).toBe(1594125399723.5461)
+  expect(firstTimestamp).toBe(1600447529443.713)
+  expect(secondTimestamp).toBe(1601383980119.988)
+  expect(secondTimestamp - firstTimestamp).toBe(diff)
+  expect(diff).toBe(936450676.2751465)
 })
