e275fa509ddaa8fd51a9d3d5a323fd535b70bb81
mornir <jeromepott@hotmail.com>
2020-10-03 17:38:52 +0200

    Add Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
   env: {
     node: true,
     webextensions: true,
+    'jest/globals': true,
   },
+  plugins: ['jest'],
   extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
     ecmaVersion: 2020,
